2971c94738b5cfae6fde1627bb64318e3913fcf8
Reuben Walker <walkerreuben@gmail.com>
2014-10-15 16:15:03 +1100

    Added week 7 notes.

---
+++ src/comp125/Week7Lecture.java
@@ -0,0 +1,47 @@
+package comp125;
+
+public class Week7Lecture {
+
+	/**
+	 * @author Reuben Walker
+	 */
+	public static void main(String[] args) {
+		/*
+		 * Post test and assignment 2 lecture. First lecture by Doctor Steve Cassidy.
+		 * Only lecture this week due to Conception Day and the mid semester test.
+		 * 
+		 * Assignment 2 will be about GPS data.
+		 * 
+		 * We'll be working with CSV data. Basic data input, but would use a comma method.
+		 * 
+		 * The problem:
+		 * Handle GPS track data
+		 * Provide sumary statistics about tracks
+		 * Sort lists of tracks for a user in different ways
+		 * Find out if a track goes close to a landmark
+		 * 
+		 * Assignment 3
+		 * Find the climbs on a track
+		 * Find whether a track follows a certain segment
+		 * Simplify a track - find the important points
+		 * Draw a track and its elevation profile.
+		 * 
+		 * A waypoint is a single GPS measurement.
+		 * A track is a collection of waypoints.
+		 * A tracklog is a collection of tracks.
+		 * 
+		 * We then define a number of methods for these classes to achieve our goals.
+		 * 
+		 * The constructor for Waypoint introduces some new ideas.
+		 * It takes a string that is a CSV representation of the waypoint, and it throws an exception if something goes wrong.
+		 * 
+		 * And exception is a way to signal an error condition from your code.
+		 * Something goes wrong so you can't do what you are supposed to do
+		 * One option would be to carry on and do the best you can
+		 * Exceptions allow you to tell your caller that you failed.
+		 * 
+		 * Check the slides for the github link.
+		 */
+	}
+
+}
