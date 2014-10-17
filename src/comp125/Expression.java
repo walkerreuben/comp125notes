d2861280ee543d063634913200cf7cbca360c3dd
Reuben Walker <walkerreuben@gmail.com>
2014-10-17 13:28:09 +1100

    Added javadoc for expression file and some javadoc notes.

--- src/comp125/Expression.java
+++ src/comp125/Expression.java
@@ -1,29 +1,57 @@
 package comp125;
 
+/**
+ * 
+ * @author Reuben Walker
+ *
+ */
 public class Expression {
-	
-	String operator;
-	Expression left;
-	Expression right;
-	int value;
-	
+
+	private String operator;
+	private Expression left;
+	private Expression right;
+	private int value;
+
+	/**
+	 * Constructor for the simplest expression, simply a number
+	 * 
+	 * @param value
+	 *            the numerical value of the expression
+	 */
 	public Expression(int value) {
 		this.value = value;
 		left = null;
 		right = null;
 		operator = null;
 	}
-	
+
+	/**
+	 * Constructor for a more complicated expression consisting of two other expressions and an operator.
+	 * 
+	 * @param l
+	 *            the left sub-expression
+	 * @param op
+	 *            the operator, either + - * or /
+	 * @param r
+	 *            the right sub-expression
+	 */
 	public Expression(Expression l, String op, Expression r) {
 		left = l;
 		operator = op;
 		right = r;
 	}
-	
+
+	public String toString() {
+		if (left == null)
+			return String.valueOf(value);
+		else
+			return "(" + left.toString() + " " + operator + " " + right.toString() + ")";
+	}
+
 	public int evaluate() throws Exception {
 		int result = 0;
 		if (left == null) {
-			//assume this is just a number
+			// assume this is just a number
 			result = value;
 		} else {
 			int lval = left.evaluate();
@@ -32,7 +60,7 @@ public class Expression {
 		}
 		return result;
 	}
-	
+
 	public int applyOperator(int lval, int rval) throws Exception {
 		int result = 0;
 		if (operator == "+")
@@ -46,12 +74,5 @@ public class Expression {
 		else
 			throw new Exception("Unknown operator: " + operator);
 		return result;
-	}
-	
-	public String toString() {
-		if (left == null)
-			return String.valueOf(value);
-		else
-			return "(" + left.toString() + " " + operator + " " + right.toString() + ")";
 	}
 }
--- src/comp125/Week9LectureTwo.java
+++ src/comp125/Week9LectureTwo.java
@@ -1,5 +1,29 @@
 package comp125;
 
 public class Week9LectureTwo {
-	
+	/*
+	 * backslash n is a new line backslash t is a tab
+	 * 
+	 * https://stackoverflow.com/questions/1367322/what-are-all-the-escape-
+	 * characters-in-java
+	 * 
+	 * Javadoc
+	 * 
+	 * Javadoc is a style of writing comments for your code that is a particular
+	 * way of writing comments to include the documentation for the code inside
+	 * the code.
+	 * 
+	 * Instead of having a separate file for your documentation, you can have
+	 * everything in the one file, which makes it easier to ensure you update
+	 * the doc whenever you change the code.
+	 * 
+	 * I should write my own CSS file for javadoc, or find a nicer looking one
+	 * than the default.
+	 * 
+	 * Can Javadoc give the index.html file a different icon to make it stick out more?
+	 * 
+	 * Don't use javadoc comments on private variables.
+	 * 
+	 * You wouldn't do javadoc comments on tests. Normal comments for these and the private variables.
+	 */
 }
