cbd3dbd5521e653b23ce8944e91bbd217b5b6afb
Reuben Walker <walkerreuben@gmail.com>
2014-10-22 13:17:34 +1100

    Added week 10 initial file.

---
+++ src/comp125/Week10LectureOne.java
@@ -0,0 +1,12 @@
+package comp125;
+
+public class Week10LectureOne {
+
+	/*
+	 * So far he's been talking about the third assignment.
+	 * 
+	 * This assignment is going to be a lot more complicated than the last one. Good.
+	 * 
+	 * 
+	 */
+}
