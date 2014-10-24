621518d17887dfb0aeb67f18cd1e3256ad870ed5
Reuben Walker <walkerreuben@gmail.com>
2014-10-24 13:33:05 +1100

    Added some main methods.

--- src/comp125/Week10LectureTwo.java
+++ src/comp125/Week10LectureTwo.java
@@ -10,9 +10,11 @@ public class Week10LectureTwo {
 		
 		if (low.size() == 0) {
 			result.addAll(high);
+			high.clear();
 			return;
 		} else if (high.size() == 0) {
 			result.addAll(low);
+			low.clear();
 			return;
 		} else if (low.getFirst() < high.getFirst()) {
 			first = low.removeFirst();
@@ -28,7 +30,15 @@ public class Week10LectureTwo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		LinkedList<Integer> one = new LinkedList<Integer>();
+		LinkedList<Integer> two = new LinkedList<Integer>();
+		LinkedList<Integer> result = new LinkedList<Integer>();
 		
+		one.add(2); one.add(5); one.add(9); one.add(10);
+		two.add(3); two.add(4); two.add(12); two.add(13); two.add(25);
+		
+		merge(one, two, result);
+		System.out.println(result);
 	}
 
 }
